4d4e1eb4648386333bdeae3faba4ca13d4c6e4e5
ecarry <lianshiliang93@gmail.com>
2025-11-09 21:54:11 +0800

    feat: add comprehensive security and profile management
    - Implemented session management with device tracking and termination capabilities
    - Added password change functionality with option to revoke other sessions
    - Created profile editing interface with avatar upload support

--- src/app/(dashboard)/dashboard/profile/page.tsx
+++ src/app/(dashboard)/dashboard/profile/page.tsx
@@ -1,17 +1,38 @@
+import { auth } from "@/modules/auth/lib/auth";
+import SecurityAccessCard from "@/modules/auth/ui/components/security-access-card";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
 export const metadata = {
-  title: "Profile",
-  description: "Profile",
+  title: "Security & Access",
 };
 
-const page = () => {
+const ProfilePage = async () => {
+  const [session, activeSessions] = await Promise.all([
+    auth.api.getSession({
+      headers: await headers(),
+    }),
+    auth.api.listSessions({
+      headers: await headers(),
+    }),
+  ]).catch(() => {
+    throw redirect("/sign-in");
+  });
+
   return (
-    <div className="py-4 px-4 md:px-8 flex flex-col gap-y-8">
-      <div>
-        <h1 className="text-2xl font-bold">Profile</h1>
-        <p className="text-muted-foreground ">Manage your profile</p>
+    <div className="flex flex-col gap-y-6 pt-2.5">
+      <div className="px-4">
+        <h1 className="text-2xl font-bold">Access & Security</h1>
+        <p className="text-xs text-muted-foreground">
+          Manage the devices logged in your account & Profile information
+        </p>
+        <SecurityAccessCard
+          session={JSON.parse(JSON.stringify(session))}
+          activeSessions={JSON.parse(JSON.stringify(activeSessions))}
+        />
       </div>
     </div>
   );
 };
 
-export default page;
+export default ProfilePage;
